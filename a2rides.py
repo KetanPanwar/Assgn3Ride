3bce704c134529a15676ae19a8587a7e70b2680f
Ketan panwar <43447711+KetanPanwar@users.noreply.github.com>
2020-03-17 20:24:28 +0530

    Update a2rides.py

--- a2rides.py
+++ a2rides.py
@@ -300,16 +300,12 @@ def count_no_of_rides():
         res1 = jsonify(res)
         # print(type(res),type(res[0]))
         resl=0
-        qres = []
         for i in res:
             if (i):
                 resl+=1
 
         # for i in res:
         #     print(i)
-        if len(qres) == 0:
-            return jsonify({}), 204
-        qres=len(qres)
         res2 = jsonify(resl)
         return res2
     else:
